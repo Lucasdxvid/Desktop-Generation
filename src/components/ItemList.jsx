ab7286bc2e9090af401cab2673e2ce07c89b2507
Lucasdxvid <athl12311@gmail.com>
2022-12-05 09:38:03 -0300

    se añade btn

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -10,12 +10,14 @@ const Product = ({ id, title, price, stock, thumbnail, description }) => {
         <p className="productStock">stock: {stock}</p>
         <div className="productContent">
           <h3 className="productTitle">{title}</h3>
-          
           <p className="productPrice">
             {" "}
             <span className="priceTag">Precio: </span> $ {price}
           </p>
         </div>
+        <button type="button" className="btn productViewBtn">
+          Ver Más
+        </button>
       </article>
     </>
   );

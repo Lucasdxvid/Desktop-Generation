91ccebff2fd1c4066de6724009e70bc8eb92997d
Lucasdxvid <athl12311@gmail.com>
2022-12-06 09:56:50 -0300

    cambios minimos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainContent from "./components/MainContent";
-// import ItemListContainer from "./components/ItemListContainer";
 import NavCustom from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/App.css";

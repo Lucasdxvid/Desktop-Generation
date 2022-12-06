8b56989a4271bd32d06d308ccc3a618f77a2520c
Lucasdxvid <athl12311@gmail.com>
2022-12-06 14:11:14 -0300

    afa

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,25 +3,27 @@ import { Link } from "react-router-dom";
 const Product = ({ id, title, price, stock, thumbnail, description }) => {
   return (
     <>
-      <article className="productCard">
-        <img
-          src={thumbnail}
-          alt="Imagen del producto"
-          className="productImage"
-        />
-        <p className="productStock">stock: {stock}</p>
-        <div className="productContent">
-          <h3 className="productTitle">{title}</h3>
-          <p className="productPrice">
-            <span className="priceTag">Precio: </span> $ {price}
-          </p>
-        </div>
-        <Link to={`/productDetail/${id}`}>
-          <button type="button" className="btn productViewBtn">
-            Ver Más
-          </button>
-        </Link>
-      </article>
+      {
+        <article className="productCard">
+          <img
+            src={thumbnail}
+            alt="Imagen del producto"
+            className="productImage"
+          />
+          <p className="productStock">stock: {stock}</p>
+          <div className="productContent">
+            <h3 className="productTitle">{title}</h3>
+            <p className="productPrice">
+              <span className="priceTag">Precio: </span> $ {price}
+            </p>
+          </div>
+          <Link to={`/productDetail/${id}`}>
+            <button type="button" className="btn productViewBtn">
+              Ver Más
+            </button>
+          </Link>
+        </article>
+      }
     </>
   );
 };

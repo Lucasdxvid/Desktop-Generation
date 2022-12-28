e6ac482119e98360a83135be09cb81c0a0f497cc
Lucasdxvid <athl12311@gmail.com>
2022-12-28 14:41:08 -0300

    detail funcional

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,20 +1,32 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { ProductsData } from "../utils/ProductsData";
-import fetchData from "../utils/FetchData";
 import ProductDetail from "./ItemDetail";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../utils/firebaseConfig";
 
 const ItemDetailContainer = () => {
   const [dato, setDato] = useState({});
   const { idProduct } = useParams();
 
   useEffect(() => {
-    fetchData(
-      2000,
-      ProductsData.find((item) => item.id === parseInt(idProduct))
-    )
+    const fetchSingleFromFireStore = async () => {
+      //Aqui necesitamos consultarle al FireStore por el producto a traves de su idProduct
+      const docRef = doc(db, "products", idProduct);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        return {
+          id: idProduct,
+          ...docSnap.data(),
+        };
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    };
+    fetchSingleFromFireStore()
       .then((result) => setDato(result))
-      .catch((err) => console.log(err));
+      .catch((error) => console.log(error));
   }, []);
 
   return <ProductDetail item={dato} />;

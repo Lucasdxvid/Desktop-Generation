74d320d58b5ef2b0e86a396f0dc6af70e9f1a9a2
Lucasdxvid <athl12311@gmail.com>
2022-12-28 14:26:27 -0300

    filtro y mapeo funcional en la list

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,23 +1,36 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../utils/firebaseConfig";
 import Product from "./ItemList";
 import Spinner from "react-bootstrap/Spinner";
 
 const ItemListContainer = () => {
-  //Aqui tenemos el estado datos del componente "ProductsData" que lo seteamos con el resultado de la promesa ("el array de productos llamado data")
   const [datos, setDatos] = useState([]);
   const { idCategory } = useParams(); // Este hook de react router nos dice cual es ese "id" que nos envian mediante la url
 
   useEffect(() => {
     const fetchFromFireStore = async () => {
-      const querySnapshot = await getDocs(collection(db, "products"));
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-      });
+      let q;
+      if (idCategory) {
+        q = query(
+          collection(db, "products"),
+          where("categoryId", "==", parseInt(idCategory))
+        );
+      } else {
+        // si es "undefined" me devuelve todos los productos al filtrar
+        q = query(collection(db, "products"));
+      }
+      const querySnapshot = await getDocs(q);
+      const dataFromFireStore = querySnapshot.docs.map((item) => ({
+        id: item.id,
+        ...item.data(),
+      })); // transformamos un array de documentos a uno de objetos
+      return dataFromFireStore;
     };
-    fetchFromFireStore();
+    fetchFromFireStore()
+      .then((result) => setDatos(result))
+      .catch((error) => console.log(error));
   }, [idCategory]); // El array vacio se comporta como un component didMount pero necesitamos que comporte como didUpdate por lo que colocamos idCategory como argumento del useEffect provocando que cada que mande otro ID se ejecute el mismo de nuevo
 
   return (

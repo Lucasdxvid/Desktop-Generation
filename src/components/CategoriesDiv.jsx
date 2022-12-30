934322e15d649fe8e20fa17401d5ad07ded0dc85
Lucasdxvid <athl12311@gmail.com>
2022-12-30 15:43:10 -0300

    cambios nombres de clases

--- src/components/CategoriesDiv.jsx
+++ src/components/CategoriesDiv.jsx
@@ -3,30 +3,30 @@ import { Link } from "react-router-dom";
 const CategoriesDiv = () => {
   return (
     <>
-      <ul className="noticiasCatUl">
+      <ul className="catUl">
       <Link to="/">
-          <li className="noticiasCatLi">
-            <label className="noticiasCatLab">Todo</label>
+          <li className="catLi">
+            <label className="catLab">Todo</label>
           </li>
         </Link>
         <Link to="/category/1">
-          <li className="noticiasCatLi">
-            <label className="noticiasCatLab">Componentes</label>
+          <li className="catLi">
+            <label className="catLab">Componentes</label>
           </li>
         </Link>
         <Link to="/category/2">
-          <li className="noticiasCatLi">
-            <label className="noticiasCatLab">Accesorios</label>
+          <li className="catLi">
+            <label className="catLab">Accesorios</label>
           </li>
         </Link>
         <Link to="/category/3">
-          <li className="noticiasCatLi">
-            <label className="noticiasCatLab">Equipos</label>
+          <li className="catLi">
+            <label className="catLab">Equipos</label>
           </li>
         </Link>
         <Link to="/category/4">
-          <li className="noticiasCatLi">
-            <label className="noticiasCatLab">Otros</label>
+          <li className="catLi">
+            <label className="catLab">Otros</label>
           </li>
         </Link>
       </ul>
